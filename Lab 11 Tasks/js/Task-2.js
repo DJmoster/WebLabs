3d8cced5e87b16af0ff5ac21a2efb8a4e5bf407c
DJmoster <Zapotichnyi12@gmail.com>
2022-10-21 19:41:41 +0300

    Lab 11 Tasks
    ## Created Task-2.js

---
+++ Lab 11 Tasks/js/Task-2.js
@@ -0,0 +1,12 @@
+
+function getNextNumber(num) {
+    if(num == 0) {
+        return 0;
+    } 
+    else if(num > 0) {
+        return num++;
+    }
+    else if(num < 0) {
+        return num--;
+    }
+}
